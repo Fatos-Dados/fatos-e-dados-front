d507d147d85784981cf6f98b963ded7f2ab2987a
Cristiano Santos <cristiano.santos@gcbinvestimentos.com>
2022-08-27 16:51:13 -0300

    style: create global style 🍷

---
+++ src/components/HelloWorld/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+    margin: 0 auto;
+    width:300px;
+    height: 500px;
+    background: linear-gradient(120deg, green, yellow);
+`
